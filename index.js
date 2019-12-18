07b19ab617dd7194b7755354c93a7c36d97bcab5
choongangkangnam2 <choongangkangnam2@kangnam2>
2019-12-18 13:51:58 +0900

    dynamic

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<body><a href="eclipse">eclipse</a></body>'))
+var downloadFolder = "C:/Users/user/Downloads/";
+var files = {
+	java: "jdk-8u231-windows-x64.exe",
+	eclipse: "eclipse-jee-2019-09-R-win32-x86_64.zip"
+};
 
-app.get('/eclipse', function(req, res) {
-	const file = '/Users/choongang/Downloads/eclipse-java-2019-09-R-win32-x86_64.zip';
-	res.download(file);
+
+
+app.get('/', (req, res) => {
+	var body = '<body>';
+	for (let name in files) {
+		body += `<a href="${name}">${name}</a><br>`;
+	}
+	body += '</body>';
+	res.send(body);
 });
+
+for (let [key, value] of Object.entries(files)) {
+	app.get(`/${key}`, function (req, res) {
+		const file = downloadFolder + value;
+		res.download(file);
+	});
+};
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
